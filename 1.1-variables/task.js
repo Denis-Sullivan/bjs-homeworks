e2e7577361aba891c6e5f59283ed69e379fee9d0
Denis-Sullivan <selivanovda@gmail.com>
2020-08-13 01:20:05 +0800

    first commit variables

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -7,21 +7,37 @@
 
 function averageMark(a, g, p) {
 	
-    // реализуйте здесь задание №1
+    // задание №1
+
+    let algebra = a;
+	let geography = g;
+	let physics = p;
+	let averageGrade  = (algebra + geography + physics) / 3;
+	
+	return averageGrade; 
     
-	return // ваш ответ;
 }
 
 function sayHello(userName) {
 	
-    // реализуйте здесь задание №2
+    // задание №2
     
-	return // ваш ответ;
+    let myName = userName;
+    let message = `Привет, мир! Меня зовут ${myName}`;
+    
+    return message;
+    
 }
 
 function calculateFormula() {
     
-	// реализуйте здесь задание №3
+    // задание №3
     
-    return // ваш ответ;
+	let x = 2;
+	let y = 22;
+	let z = 0;
+    let result = x * y + 5 * z + x - 1;
+    
+    return result;    
+
 }
